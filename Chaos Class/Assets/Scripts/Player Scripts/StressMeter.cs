d1841bb0b754935f2d737ef1c8dc814c3373a721
Osama <onagi@umich.edu>
2025-03-24 05:15:44 -0400

    Game Starts on main Menu, click play brings you to classroom scene with stress meter ui and high score ui displayed, and game over brings you to game over UI

--- Chaos Class/Assets/Scripts/Player Scripts/StressMeter.cs
+++ Chaos Class/Assets/Scripts/Player Scripts/StressMeter.cs
@@ -3,28 +3,43 @@ using UnityEngine.UI;
 
 public class StressMeter : MonoBehaviour
 {
-    public Image AddStress; 
-    private float LevelofStress = 0f; 
+    public Image AddStress;
+    private float LevelofStress = 0f;
     public float RateofIncrease = 0.1f;
-    public float MaxAmountofStress = 1f; 
+    public float MaxAmountofStress = 1f;
+
+    public GameObject Gameoverpanel;
+    public Gamestate Gamestatemanager;
 
     void Update()
     {
-        
+
         LevelofStress += RateofIncrease * Time.deltaTime;
         LevelofStress = Mathf.Clamp(LevelofStress, 0f, MaxAmountofStress);
 
-        
+
         AddStress.fillAmount = LevelofStress;
 
-        
+
         if (LevelofStress >= MaxAmountofStress)
         {
             Debug.Log("Your stress meter is maxed out");
+            Gameover();
         }
     }
 
-    
+    void Gameover()
+    {
+        Gameoverpanel.SetActive(true);
+        Cursor.visible = true;       // Show the cursor
+        Cursor.lockState = CursorLockMode.None;
+
+
+        Time.timeScale = 0f; // Pause the game
+        Debug.Log("Game Over! Game is now paused.");
+    }
+
+
     public void IncreaseStress(float amount)
     {
         LevelofStress += amount;
