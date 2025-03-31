534ce996a230972126b42b28777f5cb26463e3cd
Chandler <cjoynes@umich.edu>
2025-03-30 20:35:40 -0400

    Squashed two Chaos Class Files into One

--- Assets/Scenes/Assets/Scripts/Interactacbles/InteractableObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InteractableObject : MonoBehaviour
-{
-    public Mesh objectMesh; // The object's mesh when held
-    public Material objectMaterial; // The material (texture) of the objec
-}
