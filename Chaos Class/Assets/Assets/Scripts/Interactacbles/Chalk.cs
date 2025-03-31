f3b218a8e1b86457052923a08122473ea0d9feaa
Blake <simpblak@umich.edu>
2025-03-31 02:03:34 -0400

    scoring and animation fix
    Should be corrected

---
+++ Chaos Class/Assets/Assets/Scripts/Interactacbles/Chalk.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Chalk : InteractableObject
+{
+    [SerializeField] private Mesh chalkMesh;
+    [SerializeField] private Material chalkMaterial;
+
+    private PlayerInteract player;
+
+    public override Mesh objectMesh => chalkMesh;
+    public override Material objectMaterial => chalkMaterial;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerInteract>();
+    }
+
+    public override void Interact()
+    {
+        if (player == null) return;
+
+        if (player.ReturnIsRaycast(1000, out RaycastHit hit))
+        {
+            Chalkboard chalkboard = hit.collider.GetComponent<Chalkboard>();
+
+            if (chalkboard != null)
+            {
+                Debug.Log($"Drawing at {hit.textureCoord}");
+                chalkboard.Draw(hit.textureCoord);
+            }
+        }
+    }
+}
---
+++ Chaos Class/Assets/Assets/Scripts/Interactacbles/InteractableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InteractableObject : MonoBehaviour
+{
+    public abstract Mesh objectMesh { get; }
+    public abstract Material objectMaterial { get; }
+    public abstract void Interact();
+}
